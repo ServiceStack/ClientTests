57d284940782c43b3f3f9e9a1f67af47a5479f11
Sergey Zhukov <svg@ngs.ru>
2017-10-12 19:15:36 +0700

    Allow to run netstandard tests on android

--- ClientTest.Android/MainActivity.cs
+++ ClientTest.Android/MainActivity.cs
@@ -1,20 +1,28 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Android.App;
 using Android.OS;
-using Xamarin.Android.NUnitLite;
+using NUnit.Common;
+using NUnitLite;
+
+//using Xamarin.Android.NUnitLite;
 
 namespace ClientTest
 {
     [Activity(Label = "ClientTest", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : TestSuiteActivity
+    public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             // tests can be inside the main assembly
             //AddTest(Assembly.GetExecutingAssembly());
-            AddTest(typeof(ClientTest.JsonServiceClientTests).Assembly);
+            //AddTest(typeof(ClientTest.JsonServiceClientTests).Assembly);
             // or in any reference assemblies
             // AddTest (typeof (Your.Library.TestClass).Assembly);
+            var writer = new ExtendedTextWrapper(Console.Out);
+            var result = new AutoRun(typeof(ClientTest.JsonServiceClientTests).Assembly).Execute(new string[0], writer, Console.In);
+
+
 
             // Once you called base.OnCreate(), you cannot add more assemblies.
             base.OnCreate(bundle);
