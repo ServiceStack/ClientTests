d156b57f538eece9916b7deec654ab193d958279
Sergey Zhukov <svg@ngs.ru>
2017-11-01 03:59:08 +0700

    Add UWP Test project

---
+++ src/ClientTest.UWP/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ClientTest.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class App
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // <SNIP>
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                // ==> ADD THIS LINE <==
+                Xamarin.Forms.Forms.Init(e);
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            // <SNIP>
+        }
+
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ClientTest.UWP/UnitTestApp.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using NUnit.Runner.Services;
+using Xamarin.Forms.Platform.UWP;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ClientTest.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UnitTestsPage : WindowsPage
+
+    {
+        public UnitTestsPage()
+        {
+            this.InitializeComponent();
+
+            var nunit = new NUnit.Runner.App();
+
+            // If you want to add tests in another assembly, add a reference and
+            // duplicate the following line with a type from the referenced assembly
+            nunit.AddTestAssembly(typeof(UnitTestsPage).GetTypeInfo().Assembly);
+
+            // Do you want to automatically run tests when the app starts?
+            nunit.Options = new TestOptions
+            {
+                AutoRun = true
+            };
+
+            LoadApplication(nunit);
+        }
+    }
+}
