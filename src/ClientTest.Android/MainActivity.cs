96239ec761e37d2fc906c8b2195d307939df7617
Demis Bellot <demis.bellot@gmail.com>
2017-11-20 18:42:01 -0500

    Upgrade to ServiceStack v5 + .NET Core 2.0

--- src/ClientTest.Android/MainActivity.cs
+++ src/ClientTest.Android/MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Reflection;
+using System.IO;
+using System.Text;
 using Android.App;
 using Android.OS;
+using Android.Widget;
 using NUnit.Common;
 using NUnitLite;
 
@@ -19,13 +21,50 @@ namespace ClientTest.Android
             //AddTest(typeof(ClientTest.JsonServiceClientTests).Assembly);
             // or in any reference assemblies
             // AddTest (typeof (Your.Library.TestClass).Assembly);
-            var writer = new ExtendedTextWrapper(Console.Out);
-            var result = new AutoRun(typeof(ClientTest.Tests.JsonServiceClientTests).Assembly).Execute(new []{"--labels=All", "--teamcity"}, writer, Console.In);
-
 
 
             // Once you called base.OnCreate(), you cannot add more assemblies.
             base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            var txtOutput = (TextView)FindViewById(Resource.Id.txtOutput);
+
+            var writer = new ExtendedTextWrapper(new MultiTextWriter(new TextViewWriter(txtOutput), Console.Out));
+            var result = new AutoRun(typeof(ClientTest.Tests.JsonServiceClientTests).Assembly).Execute(new[] { "--labels=All", "--teamcity" }, writer, Console.In);
+        }
+    }
+
+    public class TextViewWriter : TextWriter
+    {
+        public override Encoding Encoding => Encoding.Default;
+
+        private readonly TextView txtView;
+        public TextViewWriter(TextView txtView) => this.txtView = txtView;
+
+        public override void Write(char value)
+        {
+            txtView.Append(value.ToString());
+        }
+
+        public override void WriteLine(string value)
+        {
+            txtView.Append(value + System.Environment.NewLine);
+        }
+    }
+
+    public class MultiTextWriter : TextWriter
+    {
+        public override Encoding Encoding => Encoding.Default;
+
+        private readonly TextWriter[] writers;
+        public MultiTextWriter(params TextWriter[] writers) => this.writers = writers;
+
+        public override void Write(char value)
+        {
+            foreach (var writer in writers)
+            {
+                writer.Write(value);
+            }
         }
     }
 }
--- src/ClientTest.Android/Resources/Resource.Designer.cs
+++ src/ClientTest.Android/Resources/Resource.Designer.cs
@@ -82,59 +82,65 @@ namespace ClientTest
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int OptionHostName = 2131034113;
-			
-			// aapt resource value: 0x7f050002
-			public const int OptionPort = 2131034114;
-			
-			// aapt resource value: 0x7f050000
-			public const int OptionRemoteServer = 2131034112;
-			
-			// aapt resource value: 0x7f050010
-			public const int OptionsButton = 2131034128;
-			
-			// aapt resource value: 0x7f05000b
-			public const int ResultFullName = 2131034123;
-			
-			// aapt resource value: 0x7f05000d
-			public const int ResultMessage = 2131034125;
-			
-			// aapt resource value: 0x7f05000c
-			public const int ResultResultState = 2131034124;
-			
-			// aapt resource value: 0x7f05000a
-			public const int ResultRunSingleMethodTest = 2131034122;
-			
-			// aapt resource value: 0x7f05000e
-			public const int ResultStackTrace = 2131034126;
-			
-			// aapt resource value: 0x7f050006
-			public const int ResultsFailed = 2131034118;
-			
 			// aapt resource value: 0x7f050003
-			public const int ResultsId = 2131034115;
-			
-			// aapt resource value: 0x7f050007
-			public const int ResultsIgnored = 2131034119;
-			
-			// aapt resource value: 0x7f050008
-			public const int ResultsInconclusive = 2131034120;
-			
-			// aapt resource value: 0x7f050009
-			public const int ResultsMessage = 2131034121;
-			
-			// aapt resource value: 0x7f050005
-			public const int ResultsPassed = 2131034117;
+			public const int OptionHostName = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int ResultsResult = 2131034116;
+			public const int OptionPort = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int OptionRemoteServer = 2131034114;
+			
+			// aapt resource value: 0x7f050012
+			public const int OptionsButton = 2131034130;
+			
+			// aapt resource value: 0x7f05000d
+			public const int ResultFullName = 2131034125;
 			
 			// aapt resource value: 0x7f05000f
-			public const int RunTestsButton = 2131034127;
+			public const int ResultMessage = 2131034127;
+			
+			// aapt resource value: 0x7f05000e
+			public const int ResultResultState = 2131034126;
+			
+			// aapt resource value: 0x7f05000c
+			public const int ResultRunSingleMethodTest = 2131034124;
+			
+			// aapt resource value: 0x7f050010
+			public const int ResultStackTrace = 2131034128;
+			
+			// aapt resource value: 0x7f050008
+			public const int ResultsFailed = 2131034120;
+			
+			// aapt resource value: 0x7f050005
+			public const int ResultsId = 2131034117;
+			
+			// aapt resource value: 0x7f050009
+			public const int ResultsIgnored = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int ResultsInconclusive = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int ResultsMessage = 2131034123;
+			
+			// aapt resource value: 0x7f050007
+			public const int ResultsPassed = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int ResultsResult = 2131034118;
 			
 			// aapt resource value: 0x7f050011
-			public const int TestSuiteListView = 2131034129;
+			public const int RunTestsButton = 2131034129;
+			
+			// aapt resource value: 0x7f050000
+			public const int SCROLLER_ID = 2131034112;
+			
+			// aapt resource value: 0x7f050013
+			public const int TestSuiteListView = 2131034131;
+			
+			// aapt resource value: 0x7f050001
+			public const int txtOutput = 2131034113;
 			
 			static Id()
 			{
@@ -150,16 +156,19 @@ namespace ClientTest
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int options = 2130903040;
+			public const int Main = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int results = 2130903041;
+			public const int options = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int test_result = 2130903042;
+			public const int results = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int test_suite = 2130903043;
+			public const int test_result = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int test_suite = 2130903044;
 			
 			static Layout()
 			{
